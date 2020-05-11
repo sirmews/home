cb1d367be5cc8245b16a5bd7969f706dd8db5c37
Nav Rao <sirmews@gmail.com>
2020-05-11 20:05:56 +1200

    prettier intro, I like to think

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
-const introText = "Hi, I'm Nav.<br/>I'm a full stack web developer.<br/>"
+const introText = "Home of Nav, <br/>a perfectly cromulent full stack developer."
+const subText = "I sometimes run meetups, write code for money and attempt poetry."
 
 const renderString = (string) => (
     string.split("<br/>").map((x, i) => {
-        return <p className="mb-4">{x.split("").map((x, i) => {
+        return <p className="mb-2">{x.split("").map((x, i) => {
+            if(x === "X") return things
             return (
                 <span key={i}>{x}</span>
             )
@@ -12,25 +14,33 @@ const renderString = (string) => (
     })
 )
 
+const links = [
+    {
+        "text": "clutter smothers, simplicity breathes",
+        "who": "terry guillemets"
+    }
+];
+
 const getThings = () => {
     return links[Math.floor(Math.random() * links.length)];
 }
 
 const Intro = () => {
-    // const [things, setThings] = useState(getThings());
+    const [things, setThings] = useState(getThings());
 
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setThings(getThings());
-    //     }, 5000);
-    // });
+    useEffect(() => {
+        setTimeout(() => {
+            setThings(getThings());
+        }, 3000);
+    });
 
     return (
         <section id="intro" className="lg:w-2/3">
             <div className="py-6 px-6 lg:px-12">
-                <div className="text-5xl animated-text font-black ">
-                    <p className="mb-4"><span className="f2">( * ^ *) <span className="wave">ノシ</span></span></p>
+                <div className="text-4xl animated-text font-black">
+                    <p className="mb-4"><span className="text-xl">( * ^ *) <span className="wave">ノシ</span></span></p>
                     { renderString(introText) }
+                    <div className="text-xl py-2">{ renderString(subText) }</div>
                 </div>
             </div>
         </section>
