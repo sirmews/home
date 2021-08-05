a24a80a6a7d246af72186f6c949e40351ceed4f1
Nav <nav@navrao.me>
2021-08-06 08:24:23 +1200

    feat: playing with animation

--- components/Pill.tsx
+++ components/Pill.tsx
@@ -1,4 +1,4 @@
-import { useSpring, animated } from 'react-spring';
+import { useSpring, animated, config } from 'react-spring';
 import getRandomColor from '../utils/getRandomColor';
 import getRandomBorderRadius from '../utils/getRandomBorderRadius';
 
@@ -8,7 +8,7 @@ const Pill = ({ pill }: any) => {
     to: { borderRadius: `${getRandomBorderRadius()} / ${getRandomBorderRadius()}` },
     from: { borderRadius: `${getRandomBorderRadius()} / ${getRandomBorderRadius()}` },
     loop: { reverse: true },
-    config: { duration: 2000, friction:20 }
+    config: { duration: 2000, mass: 1, tension: 280, friction: 60 }
   })
   return (
     <animated.span
--- utils/getRandomBorderRadius.ts
+++ utils/getRandomBorderRadius.ts
@@ -1,10 +1,12 @@
+const random = (min: number, max: number) => Math.floor(Math.random() * (max - min)) + min;
+
 const getRandomColor = () => {
 
   const numOfValues = 4;
 
   return new Array(numOfValues)
     .fill('')
-    .map(() => Math.floor(Math.random() * 100) + '%')
+    .map(() => Math.floor(random(2, 5) * 100) + '%')
     .join(' ');
 };
 
